07578c62c38b36d4d0fef21a4f72b7e587c19817
carreraSilvio <carrera.silvio@gmail.com>
2026-02-20 16:04:01 -0300

    first pass on layered fsm

--- Runtime/LayeredFSM/LayeredFSM.cs
+++ Runtime/LayeredFSM/LayeredFSM.cs
@@ -1,15 +1,62 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace BrightLib.StateMachine.Runtime
 {
     /// <summary>
     /// Manages multiple FSMs
     /// </summary>
+    [DisallowMultipleComponent]
     public sealed class LayeredFSM : MonoBehaviour
     {
-        [SerializeField]
-        public FSM[] _fsms;
+        private FSM[] _layers;
 
+        private void Start()
+        {
+            _layers = GetComponents<FSM>()
+                .OrderByDescending(fsm => fsm.Priority)
+                .ToArray();
+
+            foreach (var fsm in _layers)
+            {
+                fsm.ChangeToInitialState();
+            }
+        }
+
+        private void Update()
+        {
+            foreach (var fsm in _layers)
+            {
+                fsm.Tick();
+            }
+        }
+
+        private void LateUpdate()
+        {
+            foreach (var fsm in _layers)
+            {
+                fsm.LateTick();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            foreach (var fsm in _layers)
+            {
+                fsm.FixedTick();
+            }
+        }
+
+        public T GetLayer<T>() where T : FSM
+        {
+            foreach (var fsm in _layers)
+            {
+                if (fsm is T typed)
+                    return typed;
+            }
+
+            return null;
+        }
 
     }
 }
